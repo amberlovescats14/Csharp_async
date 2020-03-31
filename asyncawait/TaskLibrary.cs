d0eaac6ffa6df5473e3953d82d1341e7f343c18b
Amber Jones <amberlovescats14@gmail.com>
2020-03-31 07:27:05 -0500

    working with task library

---
+++ asyncawait/TaskLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace asyncawait
+{
+    public class TaskLibrary
+    {
+        public void example()
+        {
+            var req = Task.Run(() =>
+            {
+                var lines = File.ReadAllLines(@"C:\Users\amber.jones\Desktop\abc.txt");
+                return lines;
+            });
+            req.ContinueWith( t =>
+            {
+                var res = req.Result;
+            });
+        }
+    }
+}
